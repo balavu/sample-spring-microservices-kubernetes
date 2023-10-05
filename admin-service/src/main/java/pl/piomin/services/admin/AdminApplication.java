006320788265e17f662f26da5a7e3b471859ac67
Bala <116500862+balavu@users.noreply.github.com>
2023-10-05 14:34:39 +0530

    Update AdminApplication.java

--- admin-service/src/main/java/pl/piomin/services/admin/AdminApplication.java
+++ admin-service/src/main/java/pl/piomin/services/admin/AdminApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableAutoConfiguration	
 @EnableAdminServer
 @EnableDiscoveryClient
 @EnableScheduling
